f88692406c64b07ad2da5ff8b51a958f32860f46
John Smoker <testgits@yandex.ru>
2021-03-24 02:44:47 +0500

    Added keyTd and valueTd

--- src/test/resources/js/get_table_data_universal.js
+++ src/test/resources/js/get_table_data_universal.js
@@ -1,13 +1,14 @@
-function getDataFromTable(tr, td){
+function getDataFromTable(tr, td, keyTd = 0, valueTd = 1){
 jsonObject = {};
 Array.from(document.querySelectorAll(tr)).forEach(e =>{
         tds =  e.querySelectorAll(td);
-        if (typeof tds[0] != "undefined") {
-            jsonObject[tds[0].innerHTML] = tds[1].innerHTML;
+        if (typeof tds[keyTd] != "undefined") {
+            jsonObject[tds[keyTd].innerHTML] = tds[valueTd].innerHTML;
         }
     }
 );
 return JSON.stringify(jsonObject);
 }
 var trArg = arguments[0]; var tdArg = arguments[1];
+var keyTd = arguments[2]; var valueTd = arguments[3];
 return getDataFromTable(trArg, tdArg);
