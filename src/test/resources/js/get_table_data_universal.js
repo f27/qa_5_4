3e6958905f90a2c0bdf3be3e2070f7f9decd3492
John Smoker <testgits@yandex.ru>
2021-03-24 02:25:32 +0500

    style fix

--- src/test/resources/js/get_table_data_universal.js
+++ src/test/resources/js/get_table_data_universal.js
@@ -1,11 +1,11 @@
-function getDataFromTable(table, td){
+function getDataFromTable(tr, td){
 jsonObject = {};
-Array.from(document.querySelectorAll(table)).forEach(e =>{
+Array.from(document.querySelectorAll(tr)).forEach(e =>{
         tds =  e.querySelectorAll(td);
         jsonObject[tds[0].innerHTML] = tds[1].innerHTML;
     }
 );
 return JSON.stringify(jsonObject);
 }
-var tableArg = arguments[0]; var tdArg = arguments[1];
-return getDataFromTable(tableArg, tdArg);
+var trArg = arguments[0]; var tdArg = arguments[1];
+return getDataFromTable(trArg, tdArg);
