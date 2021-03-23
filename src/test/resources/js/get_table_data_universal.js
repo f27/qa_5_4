26995dd61ea9cd99d297264f41d3782f6a4300be
John Smoker <testgits@yandex.ru>
2021-03-24 02:58:00 +0500

    Removed old javascripts

--- src/test/resources/js/get_table_data_universal.js
+++ src/test/resources/js/get_table_data_universal.js
@@ -2,7 +2,7 @@ function getDataFromTable(tr, td, keyTd = 0, valueTd = 1){
 jsonObject = {};
 Array.from(document.querySelectorAll(tr)).forEach(e =>{
         tds =  e.querySelectorAll(td);
-        if (typeof tds[keyTd] != "undefined") {
+        if (typeof tds[keyTd] != "undefined") { //if th skip
             jsonObject[tds[keyTd].innerHTML] = tds[valueTd].innerHTML;
         }
     }
