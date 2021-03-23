5b523c9226ce2e78fb5a9a3ed247db2a348bbb34
John Smoker <testgits@yandex.ru>
2021-03-24 02:57:22 +0500

    Removed old javascripts

--- src/test/resources/js/get_table_data.js
+++
@@ -1,7 +0,0 @@
-jsonObject = {};
-$(".table-responsive tbody tr").each(function(i, element) {
-    key =  $(element).find("td").first().text();
-    value = $(element).find("td").last().text()
-    jsonObject[key] = value;
-});
-return JSON.stringify(jsonObject);
--- src/test/resources/js/get_table_data_no_jquery.js
+++
@@ -1,7 +0,0 @@
-jsonObject = {};
-Array.from(document.querySelectorAll(".table-responsive tbody tr")).forEach(e =>{
-        tds =  e.querySelectorAll("td");
-        jsonObject[tds[0].innerHTML] = tds[1].innerHTML;
-    }
-);
-return JSON.stringify(jsonObject);
